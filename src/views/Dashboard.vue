632f13792653baad5b64e0ecd93a1fcdc0d3ca4e
Lascoyle <meursault45@live.com>
2021-01-09 14:37:37 +0100

    Make dynamic status colors, add a sort projects functionnality

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,23 +3,37 @@
     <h1 class="subtitle-1  grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+
+      <v-layout row class="mb-3">
+        <v-btn small text color="grey" @click="sortBy('title')">
+          <v-icon left small>mdi-folder</v-icon>
+          <span class="caption text-lowercase">by project name</span>
+        </v-btn>
+        <v-btn small text color="grey" @click="sortBy('person')">
+          <v-icon left small>mdi-account</v-icon>
+          <span class="caption text-lowercase">by person</span>
+        </v-btn>
+      </v-layout>
+
       <v-card flat color="grey lighten-5" class="pa-3" v-for="(project, index) in projects" :key="index">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
-            <v-card-title class="caption grey--text">{{ project.title }}</v-card-title>
-            <v-card-text>Create a new website</v-card-text>
+            <v-card-title class="caption grey--text">Project</v-card-title>
+            <v-card-text>{{ project.title }}</v-card-text>
           </v-flex>
           <v-flex xs6 sm4 md2>
-            <v-card-title class="caption grey--text">{{ project.person }}</v-card-title>
-            <v-card-text>Lascoyle</v-card-text>
+            <v-card-title class="caption grey--text">Person</v-card-title>
+            <v-card-text>{{ project.person }}</v-card-text>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <v-card-title class="caption grey--text">{{ project.due }}</v-card-title>
             <v-card-text>15 Jan 2021</v-card-text>
           </v-flex>
           <v-flex xs6 sm4 md2>
-            <v-card-title class="caption grey--text">{{ project.status }}</v-card-title>
-            <v-card-text>Ongoing</v-card-text>
+            <div class="float-right mr-5">
+              <v-card-title class="caption grey--text pb-2">Status</v-card-title>
+              <v-chip :class="`${project.status} white--text caption`">{{ project.status }}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider class="mt-3"></v-divider>
@@ -41,20 +55,37 @@ export default {
         { title: "Create a community forum", person: "Marvin", due: "15th Nov 2020", status: "overdue" },
       ]
     }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+    }
   }
 }
 </script>
 
 <style>
 .project.complete {
-  border-left: 4px solid #3cd1c2;
+  border-left: 7px solid #3cd1c2;
 }
 
 .project.ongoing {
-  border-left: 4px solid orange;
+  border-left: 7px solid orange;
 }
 
 .project.overdue {
-  border-left: 4px solid tomato;
+  border-left: 7px solid tomato;
+}
+
+.v-chip--no-color.complete {
+  background-color: #3cd1c2!important;
+}
+
+.v-chip--no-color.ongoing {
+  background-color: orange!important;
+}
+
+.v-chip--no-color.overdue {
+  background-color: tomato!important;
 }
 </style>
