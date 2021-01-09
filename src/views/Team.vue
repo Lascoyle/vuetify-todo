52902ce91def278c6a82607c4af5dda3c87c2dda
Lascoyle <meursault45@live.com>
2021-01-09 12:02:36 +0100

    First commit, project initiation, create routes and navigation

---
+++ src/views/Team.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="team">
+    <h1 class="subtitle-1 grey--text">Team</h1>
+    <v-container class="my-5">
+      
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Team',
+  components: {
+    //
+}
+}
+</script>
