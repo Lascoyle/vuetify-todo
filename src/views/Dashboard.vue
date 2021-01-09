a49883a836bf0812507e624b1a4d56fc271f3449
Lascoyle <meursault45@live.com>
2021-01-09 12:32:33 +0100

    Initiate layout for projects dashboard with hard-coded datas

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,29 +3,26 @@
     <h1 class="subtitle-1  grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 md6 >
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2 >
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2 >
-          <v-btn outline block class="primary">3</v-btn>
-        </v-flex>
-        <v-flex xs4 md2 >
-          <v-btn outline block class="primary">4</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-center>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">5</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">6</v-btn>
-        </v-flex>
-      </v-layout>
+     <v-card flat color="grey lighten-5" class="pa-3">
+       <v-layout row wrap>
+         <v-flex xs12 md6>
+           <v-card-title class="caption grey--text">Project Title</v-card-title>
+           <v-card-text>Create a new website</v-card-text>
+         </v-flex>
+         <v-flex xs6 sm4 md2>
+            <v-card-title class="caption grey--text">Person</v-card-title>
+            <v-card-text>Lascoyle</v-card-text>
+         </v-flex>
+         <v-flex xs6 sm4 md2>
+            <v-card-title class="caption grey--text">Due by</v-card-title>
+            <v-card-text>15 Jan 2021</v-card-text>
+         </v-flex>
+         <v-flex xs6 sm4 md2>
+            <v-card-title class="caption grey--text">Status</v-card-title>
+            <v-card-text>Ongoing</v-card-text>
+         </v-flex>
+       </v-layout>
+     </v-card>
     </v-container>
 
   </div>
