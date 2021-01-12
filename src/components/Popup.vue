b45e857adddacd47ac3328f76dbf7f6ac5b660bf
Lascoyle <meursault45@live.com>
2021-01-12 17:05:13 +0100

    Add a date picker to the popup, install date-fns to format date

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,6 +11,18 @@
                 <v-form class="px-3">
                     <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
                     <v-textarea label="Informations" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+
+                    <v-menu offset-y min-width="auto" transition="scale-transition">
+                        <template v-slot:activator="{on, attrs}">
+                            <v-text-field :value="formatDate" v-bind="attrs" v-on="on" label="Due date" prepend-icon="mdi-calendar-month"></v-text-field>
+                        </template>
+                        <v-date-picker v-model="due">
+                            <v-spacer></v-spacer>
+                            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                            <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                        </v-date-picker>
+                    </v-menu>
+
                     <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
                 </v-form>
             </v-card-text>
@@ -19,16 +31,25 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
+import parseISO from 'date-fns/parseISO'
+
 export default {
     data() {
         return {
             title: '',
-            content: ''
+            content: '',
+            due: null
         }
     },
     methods: {
         submit() {
             console.log(this.title, this.content)
+        }
+    },
+    computed: {
+        formatDate() {
+            return this.due ? format(parseISO(this.due), 'eee MMM d yyyy ') : '';
         }
     }
 }
